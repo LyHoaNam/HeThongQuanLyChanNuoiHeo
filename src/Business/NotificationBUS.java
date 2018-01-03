ada6855570e6993efd6198f9fdd5e24c0f89e804
LyHoaNam <lyhoanam@gmail.com>
2018-01-03 08:03:21 +0700

    notification

---
+++ src/Business/NotificationBUS.java
@@ -0,0 +1,30 @@
+
+package Business;
+
+import Data.classData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class NotificationBUS 
+{
+    classData DB=new classData();
+    public int getGioChoAn(String idChuong)
+    {
+        String sql="SELECT * FROM giochoan WHERE id= (SELECT MAX(id) FROM giochoan) AND MaChuong='"+idChuong+"'";
+        ResultSet rs=DB.getData(sql);
+        int temp=-1;
+        try
+        {
+            while(rs.next())
+            {
+            temp=rs.getInt("GioChoAn");
+            }
+        }
+        catch(SQLException exc)
+        {
+            System.out.println("Loi o NotificationBUS "+exc);
+        }
+        return temp;
+    }
+}
